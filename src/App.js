d2acb6e12a3b909952834709fb7c75b5f3daa1ad
Nick Elobsky <echonok@gmail.com>
2022-12-02 22:07:19 +0100

    added modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/layout/Header/Header';
 import Meals from './components/meals/Meals';
+import Cart from './components/cart/Cart/Cart';
 
 function App() {
   return (
     <React.Fragment>
+      <Cart/>
       <Header/>
       <main>
         <Meals/>
