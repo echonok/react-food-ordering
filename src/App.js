e0e649e2e91414d4cfe4b5188dd6071bd0633624
Nick Elobsky <echonok@gmail.com>
2022-12-02 19:32:13 +0100

    added meals components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from './components/layout/Header/Header';
+import Meals from './components/meals/Meals';
 
 function App() {
   return (
     <React.Fragment>
       <Header/>
+      <main>
+        <Meals/>
+      </main>
     </React.Fragment>
   );
 }
