641595e163e515f8d891adcfdcb306fcb69a6b9e
Nick Elobsky <echonok@gmail.com>
2022-12-02 19:44:53 +0100

    added meal list cart

--- src/components/meals/AvailableMeals/AvailableMeals.jsx
+++ src/components/meals/AvailableMeals/AvailableMeals.jsx
@@ -1,12 +1,16 @@
 import { DUMMY_MEALS } from '../../../mock/available-meals.mock';
 import classes from './AvailableMeals.module.css';
+import Card from '../../UI/Card';
+import MealItem from '../MealItem/MealItem';
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>)
+  const mealsList = DUMMY_MEALS.map((meal) => <MealItem key={meal.id} meal={meal}/>)
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   )
 }
