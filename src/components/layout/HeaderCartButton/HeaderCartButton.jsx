80bd1aa12b5967331b0b482d2a386feff75ab891
Nick Elobsky <echonok@gmail.com>
2022-12-04 15:47:28 +0100

    added somple

--- src/components/layout/HeaderCartButton/HeaderCartButton.jsx
+++ src/components/layout/HeaderCartButton/HeaderCartButton.jsx
@@ -1,7 +1,14 @@
 import CartIcon from '../../cart/CartIcon/CartIcon';
 import classes from './HeaderCartButton.module.css';
+import { useContext } from 'react';
+import CartContext from '../../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((acc, item) => acc + item.amount, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
